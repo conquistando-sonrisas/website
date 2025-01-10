09f1f0d4daab3e1c0298131bca392f26ada23c65
Luis Cruz <luisccc99@gmail.com>
2025-01-09 21:40:02 -0600

    Agregar componentes que se utilizaran en la mayoria de paginas (footer, navbar)

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+
+export default function Footer() {
+
+  return (<Box component='footer' height='300px' sx={{ backgroundColor: 'conquiDarkBlue.main' }} >
+    <Typography>Footer links</Typography>
+  </Box>)
+}   
