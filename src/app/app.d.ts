baf50265e2c959670b7de5e0dfcda933c8813573
Luis Cruz <luisccc99@gmail.com>
2025-02-26 22:12:56 -0600

    Agregar archivo para definir tipos de datos en la aplicacion

---
+++ src/app/app.d.ts
@@ -0,0 +1,44 @@
+
+export interface MediaSize {
+  ext: string;
+  url: string;
+  mime: string;
+  name: string;
+  size: number;
+  width: number;
+  height: number;
+  sizeInBytes: number;
+}
+
+
+export interface Media {
+  id: number;
+  documentId: string;
+  name: string;
+  alternativeText: string | null;
+  caption: string | null;
+  width: number;
+  height: number;
+  ext: string;
+  url: string;
+  publishedAt: string;
+  formats: {
+    large: MediaSize;
+    small: MediaSize;
+    medium: MediaSize;
+    thumbnail: MediaSize;
+  }
+}
+
+
+export interface HomeSinglePage {
+  data: {
+    id: number;
+    documentId: string;
+    FormularioType: string;
+    NosotrosDescripcion: string;
+    publishedAt: string;
+    ConquiKid: Media;
+    NosotrosImagen: Media
+  }
+}
