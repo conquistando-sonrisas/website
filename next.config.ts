1a6d02892c3c1d605dabbebbc0ea8e46aefefb02
Luis Cruz <luisccc99@gmail.com>
2025-02-26 22:14:00 -0600

    Agregar dominio de bucket en donde se encuentran imagenes del contenido en la pagina

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'static-content-lccc.s3.us-east-1.amazonaws.com',
+    }]
+  }
 };
 
 export default nextConfig;
