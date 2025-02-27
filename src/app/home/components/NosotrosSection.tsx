772755d92c4b18beebaf550cdae9512e05d5ddab
Luis Cruz <luisccc99@gmail.com>
2025-02-26 22:19:26 -0600

    Renombrar componente Nosotros a NosotrosSection

--- src/app/home/components/NosotrosSection.tsx
+++ src/app/home/components/NosotrosSection.tsx
@@ -1,12 +1,14 @@
+'use client'
+
 import { Box, Button, Grid2, Typography } from "@mui/material";
 import Image from "next/image";
 import voluntarios from '../../../../public/voluntarios.jpg'
 import SectionTitle from "@/app/components/SectionTitle";
 import { ArrowForward } from "@mui/icons-material";
 
-export function NosotrosSection() {
+export default function NosotrosSection(props: { nosotrosImagenSrc: string, NosotrosDescripcion: string }) {
 
-  return (<Grid2 container>
+  return (<Grid2 container component='section'>
     <Grid2
       height='500px'
       size={{ xs: 12, md: 6 }}
@@ -14,7 +16,7 @@ export function NosotrosSection() {
       rowSpacing={1}
       position='relative'>
       <Image
-        src={voluntarios}
+        src={props.nosotrosImagenSrc}
         width={0}
         height={0}
         layout='fill'
@@ -24,13 +26,11 @@ export function NosotrosSection() {
     <Grid2 size={{ xs: 12, md: 'grow' }} sx={{ p: { md: 3, xs: 1 } }}>
       <SectionTitle>Nosotros</SectionTitle>
       <Typography mb={2}>
-        Somos una asociación que vela por el bienestar emocional y funcional
-        de niños, niñas y adolescentes con diagnóstico de cáncer del estado de
-        Chihuahua.
+        {props.NosotrosDescripcion}
       </Typography>
       <Button endIcon={<ArrowForward />} variant="contained" sx={{
         borderRadius: '25px'
-      }} color="conquiLightBlue">Ver más</Button>
+      }} color="conquiDarkBlue">Ver más</Button>
     </Grid2>
   </Grid2>)
 }
