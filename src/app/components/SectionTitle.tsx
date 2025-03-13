8d24d7a38ed387b2040ce9bc39198073cc853bf3
Luis Cruz <luisccc99@gmail.com>
2025-03-12 23:17:10 -0600

    Agregar prop opcional para modificar estilo del titulo de secciones en las paginas

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -1,13 +1,17 @@
 import { SxProps, Theme, Typography } from "@mui/material";
 import { ReactNode } from "react";
 
-export default function SectionTitle(props: { children: ReactNode }) {
+export default function SectionTitle(props: { children: ReactNode, sx?: SxProps }) {
   return (<Typography
     my={2}
     variant="h5"
     fontSize={32}
     color="conquiDarkBlue.main"
     fontWeight={700}
+    sx={{
+      overflowWrap: 'break-word',
+      ...props.sx
+    }}
   >
     {props.children}
   </Typography>)
