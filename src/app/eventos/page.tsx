164f168211300c5a6e43d9a0e13aaa23ae8539b2
Luis Cruz <luisccc99@gmail.com>
2025-03-23 12:35:11 -0600

    Agregar estructura basica de pagina de eventos

---
+++ src/app/eventos/page.tsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import Hero from "../components/Hero";
+
+
+export default function EventosPage() {
+
+  return (
+    <main style={{ minHeight: '100vh' }}>
+      <Hero
+        title="¡Conoce nuestros próximos eventos!"
+      />
+      <Typography textAlign={'center'}>Página en progreso 🛠️</Typography>
+    </main>
+  )
+}
