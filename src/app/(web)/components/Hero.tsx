2af3ec855dbb085e5a4d98fe02e6a50542180d77
Luis Cruz <luisccc99@gmail.com>
2025-04-23 19:56:49 -0600

    Cambiar altura del componente hero

--- src/app/(web)/components/Hero.tsx
+++ src/app/(web)/components/Hero.tsx
@@ -4,7 +4,7 @@ import { Box, Container, Typography } from "@mui/material";
 export default function Hero(props: { title: string, desc?: string }) {
 
   return (
-    <Box height='52vh' sx={{
+    <Box height='45vh' sx={{
       background: 'linear-gradient(145deg, rgba(136,147,228,1) 0%, rgba(136,192,228,1) 100%)',
       display: 'flex',
       justifyContent: 'center',
