dea821d0c5e0f477b6be54056c988c5aadb29981
Luis Cruz <luisccc99@gmail.com>
2025-05-01 23:00:17 -0600

    Ajustar tamaño del componente hero para distintos dispositivos

--- src/app/(web)/components/Hero.tsx
+++ src/app/(web)/components/Hero.tsx
@@ -4,7 +4,7 @@ import { Box, Container, Typography } from "@mui/material";
 export default function Hero(props: { title: string, desc?: string }) {
 
   return (
-    <Box height='45vh' sx={{
+    <Box height={{xs: '50vh', md: '45vh'}} sx={{
       background: 'linear-gradient(145deg, rgba(136,147,228,1) 0%, rgba(136,192,228,1) 100%)',
       display: 'flex',
       justifyContent: 'center',
@@ -14,7 +14,7 @@ export default function Hero(props: { title: string, desc?: string }) {
       <Container maxWidth='xl'>
         <Typography fontSize='3em' maxWidth='500px' fontWeight={600} zIndex={2}>{props.title}</Typography>
         {props.desc && (
-          <Typography zIndex={2}>{props.desc}</Typography>
+          <Typography zIndex={2} fontSize={{ xs: '1.2em', md: '1.5em' }}>{props.desc}</Typography>
         )}
       </Container>
     </Box>
