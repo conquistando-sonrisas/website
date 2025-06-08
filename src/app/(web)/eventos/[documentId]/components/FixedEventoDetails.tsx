fa79a802f152ae60b655b7344f65d0d4d7c17eb8
Luis Cruz <luisccc99@gmail.com>
2025-06-07 20:19:00 -0600

    Hacer que detalles de la ubicacion y hora de un evento sean fijos despues de scrollear la pagina

--- src/app/(web)/eventos/[documentId]/components/FixedEventoDetails.tsx
+++ src/app/(web)/eventos/[documentId]/components/FixedEventoDetails.tsx
@@ -3,14 +3,16 @@
 import { EventAvailable, Place } from "@mui/icons-material"
 import { Box, Button, Typography, useScrollTrigger } from "@mui/material"
 import { format, parse } from "date-fns";
-import { useEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 
 export default function EventoDetails({ fechaInicio, horaInicio, ubicacion }: { fechaInicio: string | null, horaInicio: string | null, ubicacion: string | null }) {
   const ref = useRef<HTMLDivElement | null>(null);
   const trigger = useScrollTrigger({ threshold: 630, disableHysteresis: true })
+  const [initialPosition, setInitialPosition] = useState<null | { x: number, y: number }>(null)
   const parsedFechaInicio = fechaInicio ? new Date(fechaInicio) : null;
   const parsedHoraInicio = horaInicio ? parse(horaInicio, 'H:m:s', new Date()) : null;
+
   const formattedFechaInicio = parsedFechaInicio
     ? parsedFechaInicio.toLocaleDateString('es', { timeZone: 'utc', weekday: 'long', day: 'numeric', year: 'numeric', month: 'long' })
     : 'Por definir';
@@ -18,29 +20,36 @@ export default function EventoDetails({ fechaInicio, horaInicio, ubicacion }: { 
     ? format(parsedHoraInicio, 'h:mm a')
     : 'Por definir';
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (ref.current) {
       const rect = ref.current.getBoundingClientRect()
-      console.log(rect)
+      setInitialPosition({ x: rect.x, y: rect.y })
     }
   }, [])
 
   return (
-    <Box ref={ref} bgcolor='#fbfdfe' width={{ xs: 'fit-parent', md: '330px', lg: 'fit-content' }} borderRadius={3} p={3} {...(trigger && {
-      position: { md: 'fixed' },
-      top: { md: 120 },
-      right: { md: 20, lg: 239 }
-    })}
+    <Box
+      ref={ref}
+      bgcolor='conquiLightBlue.50'
+      width={{ xs: 'fit-parent', md: '330px', lg: 'fit-content' }}
+      borderRadius={3}
+      p={3}
+      {...(trigger && {
+        position: { md: 'fixed' },
+        top: { md: 120 },
+        left: { md: initialPosition?.x }
+      })}
     >
       <Box display='flex' flexDirection='row' alignItems='center' columnGap={3}>
         <EventAvailable sx={{ fontSize: 32, color: 'conquiDarkBlue.dark' }} />
         <Typography fontWeight={500}> {formattedFechaInicio} • {formattedHoraInicio}</Typography>
       </Box>
+      {ubicacion && (
       <Box display='flex' flexDirection='row' alignItems='center' columnGap={3} mt={3}>
         <Place sx={{ fontSize: 32, color: 'conquiDarkBlue.dark' }} />
-        <Typography fontWeight={500}>{ubicacion || 'Por definir'}</Typography>
+        <Typography fontWeight={500}>{ubicacion}</Typography>
       </Box>
-      {/* <Button size="large" variant="contained" color='conquiDarkBlue' sx={{ textTransform: 'none', mt: 3 }}>Adquiere tu kit!</Button> */}
+      )}
     </Box>
   )
 }
