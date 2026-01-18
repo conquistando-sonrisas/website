780d7427a715beb7850226037238e8169e270ef7
Luis Cruz <luisccc99@gmail.com>
2026-01-18 14:04:33 -0600

    Ocultar titulo y boton por defecto de pago de mercado pago para mostrar

--- src/app/(donacion)/components/MercadoPagoPayment.tsx
+++ src/app/(donacion)/components/MercadoPagoPayment.tsx
@@ -3,22 +3,30 @@
 import { Frequency } from "@/app/(web)/app";
 import { IPaymentBrickCustomization } from "@mercadopago/sdk-react/esm/bricks/payment/type";
 import dynamic from "next/dynamic"
+import RecurrentDonation from "./RecurrentDonation";
+import OneTimeDonation from "./OneTimeDonation";
+import { Box } from "@mui/material";
+import { CoverFees } from "./CoverFees";
 
-const RecurrentDonation = dynamic(() => import('./RecurrentDonation'), { ssr: false });
-const OneTimeDonation = dynamic(() => import('./OneTimeDonation'), { ssr: false });
 
 
 export const MercadoPagoPayment = ({ frequency, amount, fees }: { frequency: Frequency, amount: number, fees: number }) => {
-  if (frequency === 'monthly') {
-    return <RecurrentDonation amount={amount} fees={fees} />
-  }
+  const DonationPayment = frequency === 'monthly'
+    ? <RecurrentDonation amount={amount} fees={fees} />
+    : <OneTimeDonation amount={amount} fees={fees} />;
 
-  return <OneTimeDonation amount={amount} fees={fees} />
+  return (
+    <Box>
+      {DonationPayment}
+    </Box>
+  )
 }
 
 
 export const paymentBrickCustomization: IPaymentBrickCustomization = {
   visual: {
+    hidePaymentButton: true,
+    hideFormTitle: true,
     texts: {
       formSubmit: 'Donar'
     } as any,
@@ -29,7 +37,7 @@ export const paymentBrickCustomization: IPaymentBrickCustomization = {
         formPadding: '10px',
         textPrimaryColor: '#151633',
         baseColor: '#24376f'
-      }
+      },
     }
   },
   paymentMethods: {
